7ff3351b099f444538bda0fe980a26109c61bedc
Vu Anh Son <24020300@vnu.edu.vn>
2025-02-28 12:38:06 +0700

    Add files via upload

---
+++ demoMain.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+// #include "demochessboard.cpp"
+// #include "demochesspiece.cpp"
+
+using namespace std;
+
+/*int main()
+{
+    Board board;
+    board.printBoard();
+
+    bool gameOver = false;
+    while (!gameOver)
+    {
+        // Input player's move
+            // Click on piece (Click on other places -> do nothing)
+            // Show valid moves of that piece
+            // Click on desired move
+        // Update board
+            // Update pieces' position
+            // Print board
+        // Check if over
+        // no -> Switch turn 
+        // yes
+            // !King.canMove() && check -> Checkmate -> Win
+            // !King.canMove() && !check -> Stalemate -> Draw
+            // moveWithoutCheckOrPawnMove = 50 -> Draw
+                // (?) quick_Draw when Insufficent Material
+        // ->
+        if (WhiteWins || BlackWins || Draw)
+        {
+            gameOver = true;
+        }
+    }
+}*/
+
+int main()
+{
+    cout << "Hello World!";
+    return 0;
+}
