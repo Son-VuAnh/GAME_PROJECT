8a865a700e101062c7e1f7fc59219293e724606c
Vu Anh Son <24020300@vnu.edu.vn>
2025-02-28 12:37:37 +0700

    Add files via upload

---
+++ demochessboard.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Board
+{
+    public:
+        Board();
+        void printBoard();
+};
+
+Board::printBoard()
+{
+    
+    cout << " A B C D E F G H";
+}
+
+
