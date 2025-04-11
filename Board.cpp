e876964eae2b896380653fc26c0a98ace3998d12
Vu Anh Son <24020300@vnu.edu.vn>
2025-04-12 04:50:28 +0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -15,22 +15,7 @@ const Piece Board::startingBoard[8][8] = {
 
 Board::Board()
 {
-    for (int y = 0; y < 8; y++)
-    {
-        for (int x = 0; x < 8; x++)
-        {
-            board[y][x] = startingBoard[y][x];
-        }
-    }
-
-    white_king_moved = black_king_moved = false;
-    white_king_x = 4; white_king_y = 7;
-    black_king_x = 4; black_king_y = 0;
-
-    white_rook_kingside_moved = white_rook_queenside_moved = false;
-    black_rook_kingside_moved = black_rook_queenside_moved = false;
-
-    last_move = Move(-1, -1, -1, -1, {EMPTY,NONE}, {EMPTY,NONE}, QUEEN);
+    reset();
 }
 
 void Board::reset()
@@ -61,9 +46,9 @@ Piece Board::check_piece_at(int x, int y) const
 void Board::make_move(const Move& move_)
 {
     // Special moves first
-    if (move_.piece_moved.type == KING && abs(move_.end_x - move_.start_x) == 2
-        && Rules::can_castle(*this, move_.piece_moved.color, move_.end_x > move_.start_x))
-    { // only need to move rook in this
+    if (move_.piece_moved.type == KING && abs(move_.end_x - move_.start_x) == 2 &&
+        Rules::can_castle(*this, move_.piece_moved.color, move_.end_x > move_.start_x))
+    {   // move rook only in this
         int rook_x = (move_.end_x > move_.start_x)? 7 : 0;
         int new_rook_x = (move_.end_x > move_.start_x)? 5 : 3;
 
@@ -72,7 +57,7 @@ void Board::make_move(const Move& move_)
     }
 
     if (move_.piece_moved.type == PAWN && Rules::can_enpassant(*this, move_)) //en passant
-    { //only need to remove the captured pawn here
+    {   //only need to remove the captured pawn here
         Move pre_move = last_move;
         board[pre_move.end_y][pre_move.end_x] = {EMPTY, NONE};
     }
@@ -86,6 +71,7 @@ void Board::make_move(const Move& move_)
         board[move_.end_y][move_.end_x].color = move_.piece_moved.color;
         board[move_.start_y][move_.start_x] = {EMPTY,NONE};
     }
+
     // Toggle king and rook's flags
     if (move_.piece_moved.type == KING)
     {
@@ -115,6 +101,7 @@ void Board::make_move(const Move& move_)
             if (move_.start_x == 0 && move_.start_y == 0) black_rook_queenside_moved = true;
         }
     }
+
     //Record last move
     last_move = move_;
 }
