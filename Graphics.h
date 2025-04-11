37c03fbda5132c0d472ccb9918784da5760ead35
Vu Anh Son <24020300@vnu.edu.vn>
2025-04-12 04:52:39 +0700

    Update Graphics.h

--- Graphics.h
+++ Graphics.h
@@ -14,17 +14,23 @@ public:
     ~Graphics();
 
     void init();
+    void display();
     void render_menu();
-    void update_board(const Board& board);
-    void display_result(const std::string& result_text);
+    void render_about();
+    void render_board(const Board& board);
+    void render_white_turn_circle();
+    void render_black_turn_circle();
+    void render_result(const std::string& result_text);
 
 private:
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* menu_texture;
+    SDL_Texture* about_texture;
     SDL_Texture* side_panel;
+    SDL_Texture* white_turn_circle;
+    SDL_Texture* black_turn_circle;
     SDL_Texture* piece_textures[12];
-
 };
 
 #endif
