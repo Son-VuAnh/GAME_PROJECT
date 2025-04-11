2514992d2380e5324c047ebfda4dd9d4abc2beab
Vu Anh Son <24020300@vnu.edu.vn>
2025-04-12 04:51:09 +0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,13 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "Board.h"
 #include "Graphics.h"
 #include "Input.h"
 #include "Rules.h"
 #include <string>
 
-enum GameState {MENU, INGAME};
+enum GameState {MENU, ABOUT, INGAME};
 
 class Game
 {
@@ -15,14 +14,14 @@ public:
     Game();
     ~Game();
 
+    GameState game_state;
     Color get_current_turn() const;
     void switch_turn();
+    void request_update();
 
     void run();
     void reset_game();
     void quit();
-
-    GameState game_state;
 
 private:
     Graphics graphics;
@@ -30,6 +29,7 @@ private:
     Color current_turn;
     Board board;
     bool running;
+    bool need_update;
     bool game_over;
     std::string result_text;
 };
