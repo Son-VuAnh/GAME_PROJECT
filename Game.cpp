08546515d34c9454551c734398273617fe5132ab
Vu Anh Son <24020300@vnu.edu.vn>
2025-04-12 04:51:31 +0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -2,22 +2,15 @@
 #include <string>
 using namespace std;
 
-Game::Game() : game_state(MENU), current_turn(WHITE), running(true), game_over(false), result_text("")
+const int BOARD_SIZE = 600;
+
+Game::Game() : game_state(MENU), current_turn(WHITE), running(true), need_update(false), game_over(false), result_text("")
 {
     graphics.init();
     sound.init();
 }
 
 Game::~Game() {}
-
-void Game::reset_game()
-{
-    board.reset();
-    current_turn = WHITE;
-    running = true;
-    game_over = false;
-    result_text = "";
-}
 
 Color Game::get_current_turn() const
 {
@@ -27,6 +20,21 @@ Color Game::get_current_turn() const
 void Game::switch_turn()
 {
     current_turn = (current_turn == WHITE)? BLACK : WHITE;
+}
+
+void Game::request_update()
+{
+    need_update = true;
+}
+
+void Game::reset_game()
+{
+    board.reset();
+    current_turn = WHITE;
+    running = true;
+    need_update = false;
+    game_over = false;
+    result_text = "";
 }
 
 void Game::quit()
@@ -44,70 +52,95 @@ void Game::run()
         if (game_state == MENU)
         {
             graphics.render_menu();
+            graphics.display();
 
             while (SDL_PollEvent(&event))
             {
                 input.handle_menu_input(event);
             }
         }
-        else
+
+        else if (game_state == ABOUT)
+        {
+            graphics.render_about();
+            graphics.display();
+
+            while (SDL_PollEvent(&event))
+            {
+                input.handle_about_input(event);
+            }
+        }
+
+        else // INGAME
         {
             while (SDL_PollEvent(&event))
             {
                 input.handle_ingame_input(event, *this, board);
             }
 
-            if (!game_over) //Check endgame conditions
+            if (need_update) // after a legal move is played -> check game ending contidions
             {
-                if (Rules::is_checkmate(board, current_turn))
+                if (!game_over)
                 {
-                    game_over = true;
-                    result_text = (current_turn == WHITE)? "Black wins by checkmate!" : "White wins by checkmate!";
+                    Color opponent_turn = (current_turn == WHITE)? BLACK : WHITE;
+                    if (Rules::is_checkmate(board, opponent_turn))
+                    {
+                        game_over = true;
+                        result_text = (opponent_turn == WHITE)? "Black wins by checkmate!" : "White wins by checkmate!";
+                        sound.play_winning_sound();
+                    }
+                    else if (Rules::is_stalemate(board, opponent_turn))
+                    {
+                        game_over = true;
+                        result_text = "Draw by stalemate!";
+                        sound.play_drawing_sound();
+                    }
+                    else if (Rules::insufficient_material(board))
+                    {
+                        game_over = true;
+                        result_text = "Draw by insufficient material!";
+                        sound.play_drawing_sound();
+                    }
+
+                    if (!game_over) switch_turn();
                 }
-                else if (Rules::is_stalemate(board, current_turn))
-                {
-                    game_over = true;
-                    result_text = "Draw by stalemate!";
-                }
-                else if (Rules::insufficient_material(board))
-                {
-                    game_over = true;
-                    result_text = "Draw by insufficient material!";
-                }
+                need_update = false;
             }
 
-            graphics.update_board(board);
+            // Display graphics
+            graphics.render_board(board);
 
             if (game_over)
             {
-                graphics.display_result(result_text);
-                if (result_text.find("wins") != string::npos)
-                {
-                    sound.play_winning_sound();
-                }
-                else if (result_text.find("Draw") != string::npos)
-                {
-                    sound.play_drawing_sound();
-                }
+                graphics.render_result(result_text);
+            }
+            else
+            {
+                if (current_turn == WHITE)
+                    graphics.render_white_turn_circle();
+                else
+                    graphics.render_black_turn_circle();
+            }
 
-                //Stay still after finishing a game
+            graphics.display();
+
+            // Stay still after finishing a game
+            if (game_over)
+            {
                 bool waiting = true;
                 while (waiting)
                 {
                     SDL_Event after_game;
-                    while(SDL_PollEvent(&after_game))
+                    while (SDL_PollEvent(&after_game))
                     {
-                        if (after_game.type == SDL_MOUSEBUTTONDOWN && after_game.button.x > 600)
+                        if (after_game.type == SDL_MOUSEBUTTONDOWN && after_game.button.x > BOARD_SIZE)
                         {
                             input.handle_ingame_input(after_game, *this, board);
                             waiting = false;
                         }
                     }
                 }
-
             }
-
-            SDL_Delay(10); // 1000/10 = 100 fps
         }
     }
 }
